e5b7e8e5b4c9a238fa57edb79efdf07173aa80e5
Daan Vandebosch <Daan Vandebosch@DESKTOP-AM7G2SU>
2018-03-19 11:06:02 +0100

    hk

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace Server
             InitializeComponent();
             MySqlConnection conn = new MySqlConnection(myConnectionString);
             MySqlCommand cmd = new MySqlCommand();
-            cmd.CommandText = "INSERT INTO tblLog (Melding) VALUES ('Luca is gay')";
+            cmd.CommandText = "INSERT INTO tblLog (Melding) VALUES ('Luca is wel gay')";
             conn.Open();
             cmd.Connection = conn;
             while (true)
