5a8362900218bf3732a51f42f89743dc39dc26da
Daan Vandebosch <Daan Vandebosch@DESKTOP-AM7G2SU>
2018-03-19 10:43:24 +0100

    kl

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace Server
             InitializeComponent();
             MySqlConnection conn = new MySqlConnection(myConnectionString);
             MySqlCommand cmd = new MySqlCommand();
-            cmd.CommandText = "INSERT INTO tblLog (Melding) VALUES ('testttt')";
+            cmd.CommandText = "INSERT INTO tblLog (Melding) VALUES ('testtt')";
             conn.Open();
             cmd.Connection = conn;
             cmd.ExecuteNonQuery();
