cced529ef848c59d8fa7e64b46080ea270fcb7a6
Daan Vandebosch <Daan Vandebosch@DESKTOP-AM7G2SU>
2018-03-19 10:47:06 +0100

    fag

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace Server
             InitializeComponent();
             MySqlConnection conn = new MySqlConnection(myConnectionString);
             MySqlCommand cmd = new MySqlCommand();
-            cmd.CommandText = "INSERT INTO tblLog (Melding) VALUES ('testtt')";
+            cmd.CommandText = "INSERT INTO tblLog (Melding) VALUES ('fag')";
             cmd.CommandText = "INSERT INTO tblLog (Melding) VALUES ('azertyuiopoiuytreza')";
             conn.Open();
             cmd.Connection = conn;
