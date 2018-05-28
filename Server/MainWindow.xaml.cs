7bf52f4ffbd34ad8d669990499165af9c7f78bf0
Daan Vandebosch <Daan Vandebosch@DESKTOP-AM7G2SU>
2018-05-28 09:16:21 +0200

    ip

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace Server
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string myConnectionString = string.Format("datasource ={0}; port=3306;username= {1};password= {2};database={3}", "10.11.51.246", "root", "bobeke", "mydb");
+        private string myConnectionString = string.Format("datasource ={0}; port=3306;username= {1};password= {2};database={3}", "10.11.51.118", "root", "bobeke", "mydb");
         //private string myConnectionString = string.Format("datasource ={0}; port=3306;username= {1};password= {2};database={3}", "localhost", "embedded", "", "embeddedwebcontrollers");
         Data sql;
         private DispatcherTimer tCheckConnectionDatabase = new DispatcherTimer();
@@ -300,6 +300,12 @@ namespace Server
             {
                 ListData.SelectedIndex = -1;
             }
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            UpdateLists();
+            updateRightList();
         }
     }
 }
